5c624897c83da8ce046a6327b84338367304875d
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2023-02-10 00:00:22 +0100

    adds cascading project delete

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -2,21 +2,22 @@ import { FaTrash } from 'react-icons/fa';
 import { useMutation } from '@apollo/client';
 import { DELETE_CLIENT } from '../mutations/clientMutations';
 import { GET_CLIENTS } from '../queries/clientQueries';
+import { GET_PROJECTS } from '../queries/projectQueries';
 
 export default function ClientRow({ client }) {
     const [deleteClient] = useMutation(DELETE_CLIENT, {
         variables: { id: client.id },
         // force a reload by re-fetching the query (has negative effects on application speed when done too often)
-        // refetchQueries: [{ query: GET_CLIENTS }]
+        refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
 
         // alternatively you can update the cache by updating the data with the return value of the mutation query of "deleteClient" in clientMutations.js
-        update(cache, { data: { deleteClient } }) {
-            const { clients } = cache.readQuery({ query: GET_CLIENTS });
-            cache.writeQuery({
-                query: GET_CLIENTS,
-                data: { clients: clients.filter(client => client.id !== deleteClient.id )},
-            });
-        }
+        // update(cache, { data: { deleteClient } }) {
+        //     const { clients } = cache.readQuery({ query: GET_CLIENTS });
+        //     cache.writeQuery({
+        //         query: GET_CLIENTS,
+        //         data: { clients: clients.filter(client => client.id !== deleteClient.id )},
+        //     });
+        // }
     });
 
     return (
